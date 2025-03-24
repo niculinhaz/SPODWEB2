8a33317e572652f7a6e8adea24194f32b348133f
Niculinhaz <132464073+niculinhaz@users.noreply.github.com>
2025-03-24 09:29:11 -0300

    reorganização dos arquivos + filtro para status

--- todoapp/js/todoapp.js
+++ todoapp/js/todoapp.js
@@ -2,7 +2,19 @@ const todos = [];
 
 document.getElementById("new-todo").addEventListener("keypress", function (e) {
     if (e.key === "Enter") addTodo();
-})
+});
+
+document.getElementById("all").addEventListener("click", function (e) {
+    renderTodos();
+});
+
+document.getElementById("pending").addEventListener("click", function (e) {
+    renderPendingTodos();
+});
+
+document.getElementById("finished").addEventListener("click", function (e) {
+    renderCompletedTodos();
+});
 
 document.getElementById("btn").addEventListener("click", function (e) {
     addTodo();
@@ -12,7 +24,7 @@ function addTodo() {
     let todoInput = document.getElementById("new-todo");
     if (todoInput.value === "") return;
 
-    let newTodo = {id: (todos.length + 1), text:(todoInput.value), completed: (false)};
+    let newTodo = { id: (todos.length + 1), text: (todoInput.value), completed: (false) };
     todos.push(newTodo);
     todoInput.value = "";
     renderTodos();
@@ -27,25 +39,48 @@ function renderTodos() {
         const todoItem = document.createElement("li");
         todoItem.id = todo.id;
         todoItem.textContent = todo.text;
-        
-        const removeTodo = document.createElement("button");
-        removeTodo.textContent = "Remover Tarefa";
 
-        removeTodo.onclick = function(e) {
-            deleteTodo(todoItem.id);
+        if (!todo.completed) {
+            const completeTodo = document.createElement("button");
+            completeTodo.id = "complete-button";
+            completeTodo.textContent = "Concluir Tarefa";
+
+            completeTodo.onclick = function (e) {
+                updateTodo(todoItem.id);
+            }
+
+            todoItem.appendChild(completeTodo);
         }
 
-        const completeTodo = document.createElement("input");
-        completeTodo.type = "checkbox";
-        
-        completeTodo.onclick = function(e) {
-            updateTodo(todoItem.id);
-        }
 
-        todoItem.appendChild(removeTodo);
-        todoItem.appendChild(completeTodo);
         todoList.appendChild(todoItem);
         count++;
+    }
+}
+
+function renderPendingTodos() {
+    const todoList = document.getElementById("todo-list");
+    todoList.innerHTML = "Tarefas Pendentes";
+    let count = 1;
+
+    for (const todo of todos) {
+        if (!todo.completed) {
+            const todoItem = document.createElement("li");
+            todoItem.id = todo.id;
+            todoItem.textContent = todo.text;
+
+            const completeTodo = document.createElement("button");
+            completeTodo.id = "complete-button";
+            completeTodo.textContent = "Concluir Tarefa";
+
+            completeTodo.onclick = function (e) {
+                updateTodo(todoItem.id);
+            }
+
+            todoItem.appendChild(completeTodo);
+
+            todoList.appendChild(todoItem);
+        }
     }
 }
 
@@ -59,9 +94,9 @@ function renderCompletedTodos() {
             const todoItem = document.createElement("li");
             todoItem.id = todo.id;
             todoItem.textContent = todo.text;
-    
+
             todoList.appendChild(todoItem);
-        }        
+        }
     }
 }
 
@@ -69,17 +104,18 @@ function updateTodo(todoId) {
     let index = todos.findIndex((todo) => {
         return todo.id == todoId;
     })
-    console.log(index)
+    let completeButton = document.getElementById(todoId).lastChild;
+    completeButton.remove();
     todos[index].completed = !todos[index].completed;
 }
 
-function deleteTodo(todoId) {
-    let index = todos.findIndex((todo) => {
-        return todo.id == todoId;
-    })
-    todos.splice(index, 1);
-    renderTodos();
-}
+// function deleteTodo(todoId) {
+//     let index = todos.findIndex((todo) => {
+//         return todo.id == todoId;
+//     })
+//     todos.splice(index, 1);
+//     renderTodos();
+// }
 
 
 // document.getElementById("new-todo").addEventListener("keypress", function(e) {
